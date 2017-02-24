df4ddc0c7454ec48474c273f23923f78f7518bfd
Dan Roberts <dan@adadevelopersacademy.org>
2017-02-24 11:57:06 -0800

    Finish instructions and comment out methods that need to be commented

--- test-file.rb
+++ test-file.rb
@@ -5,20 +5,24 @@
 
 # Task 1: One partner should uncomment this method, commit the changes
 # and push to GitHub. The other partner should pull the changes down.
-def duck_noise
-  puts "quack"
-end
+# def duck_noise
+#   puts "quack"
+# end
 
 # Task 2: Same as task one, but switch roles
-def truck_noise
-  puts "honk"
-end
+# def truck_noise
+#   puts "honk"
+# end
 
 # Task 3:
 #   1. Partner 1: Change this method to puts "tick", commit and push.
 #   2. Partner 2: *Before pulling*, change this method to "tock" and commit.
 #   3. Partner 2: Try to push. Git should warn you that your local copy is out of date.
-#   3. Partner 2: Now run git pull.
+#   4. Partner 2: Now run git pull. There's a merge conflict!
+#   5. Partner 2: Work with your partner to resolve the conflict.
+#                 - Use git add to mark a conflict as resolved.
+#                 - Once you're done, use git commit to finish the merge.
+#                 - Use git push to send the resolved changes up to GitHub.
 def clock_noise
   puts ""
 end
