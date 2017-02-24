4f3fcaf0e1982d0771983d75c1e9127638e15fac
Dan Roberts <dan@adadevelopersacademy.org>
2017-02-24 15:43:53 -0800

    Finalize all tasks

--- test-file.rb
+++ test-file.rb
@@ -3,8 +3,10 @@
 # of code via git. In the interest of time we'll be uncommenting
 # code rather than writing it, but the same principals will apply.
 
-# Task 1: One partner should uncomment this method, commit the changes
-# and push to GitHub. The other partner should pull the changes down.
+# Task 1: Pulling in changes
+# One partner should uncomment this method, add and
+# commit the changes and push to GitHub. The other
+# partner should run git pull to retrieve the changes.
 # def duck_noise
 #   puts "quack"
 # end
@@ -14,17 +16,23 @@
 #   puts "honk"
 # end
 
-this is a test!
+# Taks 3: Merging Changes
+# One partner should uncomment one method, and the other
+# partner should uncomment the other. Git will merge the two
+# sets of changes together.
 
-# Task 3:
-#   1. Partner 1: Change this method to puts "tick", commit and push.
-#   2. Partner 2: *Before pulling*, change this method to "tock" and commit.
-#   3. Partner 2: Try to push. Git should warn you that your local copy is out of date.
-#   4. Partner 2: Now run git pull. There's a merge conflict!
-#   5. Partner 2: Work with your partner to resolve the conflict.
-#                 - Use git add to mark a conflict as resolved.
-#                 - Once you're done, use git commit to finish the merge.
-#                 - Use git push to send the resolved changes up to GitHub.
+# def robot_noise
+#   puts "beep boop"
+# end
+
+# def train_noise
+#   puts "choo choo"
+# end
+
+# Task 4: Merge Conflict
+# Both partners should modify the same line in different ways.
+# Git doesn't know what to do in this case, so you'll have to
+# merge the two versions by hand.
 def clock_noise
   puts ""
 end
