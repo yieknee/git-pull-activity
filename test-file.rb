535a87491cd9844f6df6da2bc89cf669ee7a46ee
Dan Roberts <dan@adadevelopersacademy.org>
2017-02-24 11:47:19 -0800

    Add instructions

---
+++ test-file.rb
@@ -0,0 +1,23 @@
+# Welcome to the git pull activity! This file contains a bunch of
+# steps to demonstrate the process of working together on a piece
+# of code via git. In the interest of time we'll be uncommenting
+# code rather than writing it, but the same principals will apply.
+
+# Task 1: One partner should uncomment this method, commit the changes
+# and push to GitHub. The other partner should pull the changes down.
+def duck_noise
+  puts "quack"
+end
+
+# Task 2: Same as task one, but switch roles
+def truck_noise
+  puts "honk"
+end
+
+# Task 3:
+#   1. Partner 1: change this method to puts "tick", commit and push
+#   2. Partner 2: *before pulling*, change this method to "tock" and commit. Don't push yet.
+#   3. Partner 2: now run git pull.
+def clock_noise
+  puts "tock"
+end
